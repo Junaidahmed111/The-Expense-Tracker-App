49654645f01aad19bf1809c323e92e1c941d0d06
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-04 16:39:13 +0500

    expense related components

--- components/Expenses Output/ExpensesOutput.js
+++ components/Expenses Output/ExpensesOutput.js
@@ -2,11 +2,11 @@ import { View, Text } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({ expenses }) {
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
-      <ExpensesList/>
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList />
     </View>
   );
 }
--- components/Expenses Output/ExpensesSummary.js
+++ components/Expenses Output/ExpensesSummary.js
@@ -1,11 +1,15 @@
 import { View, Text } from "react-native";
-function ExpensesSummary() {
+function ExpensesSummary({ expenses, periodName }) {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
   return (
     <View>
-      <Text>Last 7 Days</Text>
-      <Text>$180.55</Text>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
       LIST OF EXPENSES
     </View>
   );
 }
 export default ExpensesSummary;
+    

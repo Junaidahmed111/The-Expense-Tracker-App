01c9393f7a57bedf80000d1400d6750943043067
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-05 22:25:36 +0500

    editing dif modes through useEffect

--- components/Expenses Output/ExpenseItem.js
+++ components/Expenses Output/ExpenseItem.js
@@ -3,10 +3,12 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormttedDate } from "../../util/date";
 import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({ id, description, amount, date }) {
   const navigation = useNavigation();
   function expensePressHandler() {
-    navigation.navigate("ManageExpense");
+    navigation.navigate("ManageExpense", {
+      expenseId: id,
+    });
   }
   return (
     <Pressable
--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -1,5 +1,14 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
-function ManageExpense() {
-  return <Text>Manage Expense Screen</Text>;
+function ManageExpense({ route, navigation }) {
+  const editedexpenseId = route.params?.expenseId;
+  const isEditing = !!editedexpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [isEditing, navigation]);
+  return;
 }
 export default ManageExpense;

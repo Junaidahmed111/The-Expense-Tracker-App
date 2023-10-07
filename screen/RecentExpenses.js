882ed1b7256e2b51bfa3069ebeeea56c3a251912
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-07 17:09:49 +0500

    updating expenses

--- screen/RecentExpenses.js
+++ screen/RecentExpenses.js
@@ -9,7 +9,7 @@ function RecentExpenses() {
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const date7daysAgo = getDateMinusDays(today, 7);
-    return expense.date > date7daysAgo;
+    return expense.date >= date7daysAgo && (expense.date<= today);
   });
   return (
     <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 days" />

de944f2ebc42cde95448b0642a2989f0f7d5f2d4
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-07 17:06:54 +0500

    delting expenses

--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -1,10 +1,13 @@
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import Button from "../components/UI/Button";
 import { GlobalStyles } from "../constants/styles";
+import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+
   const editedexpenseId = route.params?.expenseId;
   const isEditing = !!editedexpenseId;
 
@@ -15,12 +18,26 @@ function ManageExpense({ route, navigation }) {
   }, [isEditing, navigation]);
 
   function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedexpenseId);
     navigation.goBack();
   }
   function cancelHanlder() {
     navigation.goBack();
   }
   function confirmHanlder() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedexpenseId, {
+        description: "Test!!!!!!!!",
+        amount: 99.99,
+        date: new Date("2023-10-5"),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: "Test",
+        amount: 19.99,
+        date: new Date("2022-10-5"),
+      });
+    }
     navigation.goBack();
   }
 

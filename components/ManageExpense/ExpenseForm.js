751efeac7f7d7afb3d8800d09aba7b0ebd453406
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-15 21:21:16 +0500

    overall form created

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,3 +1,28 @@
-function ExpenseForm() {}
+import { View } from "react-native";
+import Input from "./Input";
+
+function ExpenseForm() {
+  function amountChangedHandler() {}
+  return (
+    <View>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input label="Description" />
+    </View>
+  );
+}
 
 export default ExpenseForm;
--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -4,6 +4,7 @@ import IconButton from "../components/UI/IconButton";
 import Button from "../components/UI/Button";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -44,7 +45,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-    <TextInput/>
+    <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHanlder}>
           Cancel

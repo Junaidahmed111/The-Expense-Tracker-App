db092b727a7ff79556ee037e2d25cc1f32f7683a
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-10-15 21:45:49 +0500

    styles added to add expense

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,11 +1,41 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function Input({ label, textInputConfig }) {
+  let inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 }
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
